c0eef973ef4d880d88d8f25808f4b7941f3bef2d
Alan Jordan <alanpjordan@gmail.com>
2017-04-10 19:59:55 +1200

    Working out first bit of JS in psuedocode in JS file

---
+++ js/calculator.js
@@ -0,0 +1,10 @@
+// User story one
+// perform basic math using the calculator
+
+// Needs eventlisteners on all the numbers
+// And event listeners on all the other buttons
+// When the buttons are clicked
+// Needs to add the nuber to a variable
+// Then neds to work out what arithmetic operation to perform
+// Then the other numbers
+// then return the total to the html in the total div
